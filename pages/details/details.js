52f7b84f3788710a0f56796413e1f8afb67262a5
Decml <454580993@qq.com>
2019-10-12 17:29:32 +0800

    购买卡片页面
    购买卡片页面 卡片选项点击购买逻辑

--- pages/details/details.js
+++ pages/details/details.js
@@ -5,62 +5,84 @@ Page({
    * 页面的初始数据
    */
   data: {
+    bigImg: "../../image/show03.jpg",
+    cardList: ["../../image/show01.jpg",
+      "../../image/show02.jpg",
+      "../../image/show03.jpg", "../../image/show04.jpg",
+      "../../image/show05.jpg", "../../image/show06.jpg"
+    ],
+    moneyList: ["50", "80", "100", "200", "300", "500"],
+    defaultIndex : 0,
+    moneyIndex:0
+  },
 
+  cardTap : function(e){
+    console.log(e.currentTarget.dataset);
+    this.setData({
+      defaultIndex: e.currentTarget.dataset.index,
+      bigImg: this.data.cardList[e.currentTarget.dataset.index]
+    })
+  },
+
+  moneyTap : function(e){
+    this.setData({
+      moneyIndex:e.currentTarget.dataset.index
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
